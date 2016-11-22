076564e4146cd672d8b03d1b23310fe2adbe8cd4
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2016-11-22 13:21:22 +0100

    abstract superclass for mongo database wrapper tests

--- com.examples.school/src/test/java/com/examples/school/mongo/MongoDatabaseWrapperTest.java
+++ com.examples.school/src/test/java/com/examples/school/mongo/MongoDatabaseWrapperTest.java
@@ -1,65 +1,16 @@
 package com.examples.school.mongo;
 
-import static org.junit.Assert.*;
-
-import java.net.UnknownHostException;
-
-import org.junit.Before;
-import org.junit.Test;
-
-import com.examples.school.Student;
-import com.examples.school.helper.MongoTestHelper;
+import com.examples.school.common.AbstractMongoDatabaseWrapperTest;
 import com.github.fakemongo.Fongo;
 import com.mongodb.MongoClient;
 
-public class MongoDatabaseWrapperTest {
+public class MongoDatabaseWrapperTest extends AbstractMongoDatabaseWrapperTest {
 
-	// SUT
-	private MongoDatabaseWrapper mongoDatabase;
-
-	// helper for testing with Mongo
-	private MongoTestHelper mongoTestHelper;
-
-	@Before
-	public void initDB() throws UnknownHostException {
-		// in-memory java implementation of MongoDB
-		// so that we don't need to install MongoDB in our computer
+	@Override
+	public MongoClient createMongoClient() {
 		Fongo fongo = new Fongo("mongo server 1");
 		MongoClient mongoClient = fongo.getMongo();
-		mongoTestHelper = new MongoTestHelper(mongoClient);
-
-		mongoDatabase = new MongoDatabaseWrapper(mongoClient);
-	}
-
-	@Test
-	public void testGetAllStudentsEmpty() {
-		assertTrue(mongoDatabase.getAllStudentsList().isEmpty());
-	}
-
-	@Test
-	public void testGetAllStudentsNotEmpty() {
-		mongoTestHelper.addStudent("1", "first");
-		mongoTestHelper.addStudent("2", "second");
-
-		assertEquals(2, mongoDatabase.getAllStudentsList().size());
-	}
-
-	@Test
-	public void testFindStudentByIdNotFound() {
-		mongoTestHelper.addStudent("1", "first");
-
-		assertNull(mongoDatabase.findStudentById("2"));
-	}
-
-	@Test
-	public void testFindStudentByIdFound() {
-		mongoTestHelper.addStudent("1", "first");
-		mongoTestHelper.addStudent("2", "second");
-
-		Student findStudentById = mongoDatabase.findStudentById("2");
-		assertNotNull(findStudentById);
-		assertEquals("2", findStudentById.getId());
-		assertEquals("second", findStudentById.getName());
+		return mongoClient;
 	}
 
 }
